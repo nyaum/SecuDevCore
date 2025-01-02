68211bbb007aa34b161c67c0be3997c2fb481ec7
문현준 <mhj45072@secuidea.com>
2025-01-02 16:30:21 +0900

    환경설정 페이지 작업중

---
+++ SecuDevCore/Controllers/ConfigController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecuDevCore.Controllers
+{
+    public class ConfigController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
